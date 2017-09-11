e1506a1a56483d16dfda2f60d8413ea3e8657929
LZRun <975476411@qq.com>
2017-09-11 17:29:56 +0800

    添加几种动画应用

---
+++ CoreAnimationDemo/AnimationExample/ZRLoadingHUDViewController.m
@@ -0,0 +1,37 @@
+//
+//  ZRLoadingHUDViewController.m
+//  CoreAnimationDemo
+//
+//  Created by GKY on 2017/9/11.
+//  Copyright © 2017年 Run. All rights reserved.
+//
+
+#import "ZRLoadingHUDViewController.h"
+
+@interface ZRLoadingHUDViewController ()
+
+@end
+
+@implementation ZRLoadingHUDViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
