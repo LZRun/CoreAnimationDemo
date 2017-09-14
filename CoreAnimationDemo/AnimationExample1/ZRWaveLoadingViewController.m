980b7b3f68bafc30289b3f32bb27a1c854ce2fdc
LZRun <975476411@qq.com>
2017-09-14 17:23:10 +0800

    优化波浪加载

--- CoreAnimationDemo/AnimationExample1/ZRWaveLoadingViewController.m
+++ CoreAnimationDemo/AnimationExample1/ZRWaveLoadingViewController.m
@@ -21,7 +21,20 @@
     ZRWaveLoadingView *loadingView = [[ZRWaveLoadingView alloc]init];
     loadingView.center = self.view.center;
     [self.view addSubview:loadingView];
-    // Do any additional setup after loading the view.
+    
+    __block CGFloat progress = 0;
+    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue());
+    dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC, 0 * NSEC_PER_SEC);
+    dispatch_source_set_event_handler(timer, ^{
+        if (progress >= 1) {
+            dispatch_cancel(timer);
+            return ;
+        }
+        progress += 0.1;
+        loadingView.progress = progress;
+    });
+
+    dispatch_resume(timer);
 }
 
 - (void)didReceiveMemoryWarning {
